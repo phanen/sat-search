a16738052fef042f8bb9d8e474c58c5d2c8b5d5c
phanium <nxhsdu@qq.com>
2024-02-29 14:16:50 +0800

    refactor
    strbuf

--- common.py
+++ common.py
@@ -1,34 +1,241 @@
 import os
 import re
+import time
 from subprocess import DEVNULL, Popen
+
+FullRound = 32
+
+SearchRoundStart = 1
+SearchRoundEnd = 10
+InitialLowerBound = 0
+
+GroupConstraintChoice = 1
+
+# Parameters for choice 1
+GroupNumForChoice1 = 1
+
+# DiffActiveSbox
+DiffActiveSbox = FullRound * [0]
+
 
 VERBOSE = 1
 SATOUTPUT = 1
-p = print if VERBOSE else lambda *args: None
+log = print if VERBOSE else lambda *_: None
 
-CADICAL_PATH = f"{os.environ['HOME']}/b/cadical/build/cadical"
-TIME_PATH = "RunTimeSummarise.out"
-MATSUICONDITION_PATH = "MatsuiCondition.out"
+CADICAL_EXE = os.path.expanduser("~/b/cadical/build/cadical")
+TIME_OUT = "RunTimeSummarise.out"
+MATSUI_OUT = "MatsuiCondition.out"
 
 
 def satsolver(ifilename, ofilename):
     # https://github.com/arminbiere/cadical/blob/e71bd58937e6513f71bd8c93d91578785c592721/src/cadical.hpp#L478
     with open(ofilename, "+w") as f:
         out = f if SATOUTPUT else DEVNULL
-        child = Popen([CADICAL_PATH, "-q", ifilename], stdout=out)
+        child = Popen([CADICAL_EXE, "-q", ifilename], stdout=out)
         child.wait()
         return child.returncode == 10
 
 
-def CountClausesInSequentialEncoding(n, k):
+def clause_counter(round, activeSbox, matsuiRoundIndex, matsuiCount):
+    def CountClausesInRoundFunction(round):
+        return 1 + round * 16 * 43
+
+    def CountClausesInSequentialEncoding(n, k):
+        """
+        n: mainVarNum
+        k: cardinalitycons
+        """
+        if k > 0:
+            return 1 + (k - 1) + (n - 2) * 3 + (k - 1) * (n - 2) * 2 + 1
+        else:
+            return n
+
+    def CountClausesForMatsuiStrategy(n, k, l, r, m):
+        count = 0
+        if m > 0:
+            if (l == 0) and (r < n - 1):
+                for _ in range(1, r + 1):
+                    count += 1
+            elif (l > 0) and (r == n - 1):
+                for _ in range(0, k - m):
+                    count += 1
+                for _ in range(0, k - m + 1):
+                    count += 1
+            elif (l > 0) and (r < n - 1):
+                for _ in range(0, k - m):
+                    count += 1
+        if m == 0:
+            for _ in range(l, r + 1):
+                count += 1
+        return count
+
+    count_clause = CountClausesInRoundFunction(round)
+    mainVarNum = 16 * round
+    cardinalityCons = activeSbox
+    count_clause += CountClausesInSequentialEncoding(mainVarNum, cardinalityCons)
+    for mc in range(matsuiCount):
+        StartRound = matsuiRoundIndex[mc][0]
+        EndRound = matsuiRoundIndex[mc][1]
+        leftNode = 16 * StartRound
+        rightNode = 16 * EndRound - 1
+        partialCardinalityCons = (
+            activeSbox - DiffActiveSbox[StartRound] - DiffActiveSbox[round - EndRound]
+        )
+        count_clause += CountClausesForMatsuiStrategy(
+            mainVarNum, cardinalityCons, leftNode, rightNode, partialCardinalityCons
+        )
+    return count_clause
+
+
+def GenSequentialEncoding(x, u, n, k):
     """
+    x: mainVars
+    u: var_u
     n: mainVarNum
     k: cardinalitycons
     """
+    seq = []
     if k > 0:
-        return 1 + (k - 1) + (n - 2) * 3 + (k - 1) * (n - 2) * 2 + 1
-    else:
-        return n
+        seq.append(f"-{x[0] + 1} {u[0][0] + 1} 0\n")
+        for j in range(1, k):
+            seq.append(f"-{u[0][j] + 1} 0\n")
+        for i in range(1, n - 1):
+            seq.append(f"-{x[i] + 1} {u[i][0] + 1} 0\n")
+            seq.append(f"-{u[i - 1][0] + 1} {u[i][0] + 1} 0\n")
+            seq.append(f"-{x[i] + 1} -{u[i - 1][k - 1] + 1} 0\n")
+        for j in range(1, k):
+            for i in range(1, n - 1):
+                seq.append(f"-{x[i] + 1} -{u[i - 1][j - 1] + 1} {u[i][j] + 1} 0\n")
+                seq.append(f"-{u[i - 1][j] + 1} {u[i][j] + 1} 0\n")
+        seq.append(f"-{x[n - 1] + 1} -{u[n - 2][k - 1] + 1} 0\n")
+    else:  # if k == 0:
+        for i in range(n):
+            seq.append(f"-{x[i] + 1} 0\n")
+    return seq
+
+
+def GenMatsuiConstraint(x, u, n, k, l, r, m):
+    seq = []
+    if m > 0:
+        if l == 0 and r < n - 1:
+            for i in range(1, r + 1):
+                seq.append(f"-{x[i] + 1} -{u[i - 1][m - 1] + 1} 0\n")
+        if l > 0 and r == n - 1:
+            for i in range(k - m):
+                seq.append(f"{u[l - 1][i] + 1} -{u[r - 1][i + m] + 1} 0\n")
+            for i in range(k - m + 1):
+                seq.append(
+                    f"{u[l - 1][i] + 1} -{x[r] + 1} -{u[r - 1][i + m - 1] + 1} 0\n"
+                )
+        if l > 0 and r < n - 1:
+            for i in range(k - m):
+                seq.append(f"{u[l - 1][i] + 1} -{u[r][i + m] + 1} 0\n")
+    else:  # if m == 0:
+        for i in range(l, r + 1):
+            seq.append(f"-{x[i] + 1} 0\n")
+    return seq
+
+
+def matsui(round):
+    matsuiRoundIndex, matsuiCount = [], 0
+    matsuiCount = 0
+    # Generate Matsui condition under choice 1
+    if GroupConstraintChoice == 1:
+        for group in range(GroupNumForChoice1):
+            for round in range(1, round - group + 1):
+                matsuiRoundIndex.append([group, group + round])
+                matsuiCount += 1
+    # Printing Matsui conditions
+    with open(MATSUI_OUT, "a") as file:
+        resultseq = f"Round: {round}; Partial Constraint Num: {matsuiCount}\n"
+        file.write(resultseq)
+        file.write(f"{matsuiRoundIndex}\n")
+        log(resultseq.strip("\n"))
+        log(f"{matsuiRoundIndex}")
+    return matsuiRoundIndex, matsuiCount
+
+
+def cnfbuilder(
+    round,
+    activeSbox,
+    matsuiRoundIndex,
+    matsuiCount,
+    SymbolicCNFConstraintForSbox,
+    cnffile,
+):
+    xi = []
+    w = []
+    xo = []
+    totalSbox = 16 * round
+    count_var = 0
+    var_u = []
+    for i in range(round):
+        xi.append(list(range(count_var, count_var + 64)))
+        count_var += 64
+        w.append(list(range(count_var, count_var + 16)))
+        count_var += 16
+    for i in range(round - 1):
+        xo.append(xi[i + 1])
+    xo.append(list(range(count_var, count_var + 64)))
+    count_var += 64
+    for i in range(totalSbox - 1):
+        var_u.append(range(count_var, count_var + activeSbox))
+        count_var += activeSbox
+
+    # Add constraints to claim nonzero input difference
+    seq = []
+    for i in range(64):
+        seq.append(f"{xi[0][i] + 1} ")
+    seq.append(f"0\n")
+    # Add constraints for the round function
+    for r in range(round):
+        y = [xo[r][P[i]] for i in range(64)]
+        for i in range(16):
+            for j in range(43):
+                X = []
+                for k in range(4):
+                    X.append(xi[r][4 * i + k])
+                for k in range(4):
+                    X.append(y[4 * i + k])
+                X.append(w[r][i])
+                for k in range(len(SymbolicCNFConstraintForSbox[0])):
+                    if SymbolicCNFConstraintForSbox[j][k] == 1:
+                        seq.append(f"-{X[k] + 1} ")
+                    if SymbolicCNFConstraintForSbox[j][k] == 0:
+                        seq.append(f"{X[k] + 1} ")
+                seq.append(f"0\n")
+
+    # Add constraints for the original sequential encoding
+    mainVars = []
+    for r in range(round):
+        for i in range(16):
+            mainVars.append(w[round - 1 - r][i])
+    mainVarNum = 16 * round
+    cardinalityCons = activeSbox
+    seq += GenSequentialEncoding(mainVars, var_u, mainVarNum, cardinalityCons)
+    # Add constraints for Matsui's strategy
+    for mc in range(matsuiCount):
+        StartRound = matsuiRoundIndex[mc][0]
+        EndRound = matsuiRoundIndex[mc][1]
+        leftNode = 16 * StartRound
+        rightNode = 16 * EndRound - 1
+        partialCardinalityCons = (
+            activeSbox - DiffActiveSbox[StartRound] - DiffActiveSbox[round - EndRound]
+        )
+        seq += GenMatsuiConstraint(
+            mainVars,
+            var_u,
+            mainVarNum,
+            cardinalityCons,
+            leftNode,
+            rightNode,
+            partialCardinalityCons,
+        )
+    with open(cnffile, "w") as file:
+        # count_clause = clause_counter(round, activeSbox, matsuiRoundIndex, matsuiCount)
+        count_clause = "".join(seq).count("\n")
+        file.write(f"p cnf {count_var} {count_clause}\n")
+        file.write("".join(seq))
 
 
 def grep(pattern, path):
@@ -37,25 +244,24 @@ def grep(pattern, path):
     return re.match(pattern, content)
 
 
-def CountClausesForMatsuiStrategy(n, k, l, r, m):
-    count = 0
-    if m > 0:
-        if (l == 0) and (r < n - 1):
-            for _ in range(1, r + 1):
-                count += 1
-        elif (l > 0) and (r == n - 1):
-            for _ in range(0, k - m):
-                count += 1
-            for _ in range(0, k - m + 1):
-                count += 1
-        elif (l > 0) and (r < n - 1):
-            for _ in range(0, k - m):
-                count += 1
-    if m == 0:
-        for _ in range(l, r + 1):
-            count += 1
-    return count
+def gen_timer():
+    timetable = {}
 
+    def tick(name):
+        t = time.time()
+        if name in timetable:
+            timetable[name].append(t)
+        else:
+            timetable[name] = [t]
+        return t
+
+    def cost(name):
+        return timetable[name][1] - timetable[name][0]
+
+    return tick, cost
+
+
+tick, cost = gen_timer()
 
 # fmt: off
 P = [
--- d-sbox.py
+++ d-sbox.py
@@ -1,219 +1,37 @@
-import cProfile
-import time
-from multiprocessing import Pool
-
 from common import *
 
-FullRound = 32
-
-SearchRoundStart = 1
-SearchRoundEnd = 10
-InitialLowerBound = 0
-
-GroupConstraintChoice = 1
-
-# Parameters for choice 1
-GroupNumForChoice1 = 1
-
-DiffActiveSbox = FullRound * [0]
+SymbolicCNFConstraintForSbox = SymbolicCNFConstraintForSbox43
 
 
-def CountClausesInRoundFunction(Round):
-    return 1 + Round * 16 * 43
-
-
-def GenSequentialEncoding(x, u, n, k, fw):
-    """
-    x: mainVars
-    u: var_u
-    n: mainVarNum
-    k: cardinalitycons
-    """
-    if k > 0:
-        fw(f"-{x[0] + 1} {u[0][0] + 1} 0\n")
-        for j in range(1, k):
-            fw(f"-{u[0][j] + 1} 0\n")
-        for i in range(1, n - 1):
-            fw(f"-{x[i] + 1} {u[i][0] + 1} 0\n")
-            fw(f"-{u[i - 1][0] + 1} {u[i][0] + 1} 0\n")
-            fw(f"-{x[i] + 1} -{u[i - 1][k - 1] + 1} 0\n")
-        for j in range(1, k):
-            for i in range(1, n - 1):
-                fw(f"-{x[i] + 1} -{u[i - 1][j - 1] + 1} {u[i][j] + 1} 0\n")
-                fw(f"-{u[i - 1][j] + 1} {u[i][j] + 1} 0\n")
-        fw(f"-{x[n - 1] + 1} -{u[n - 2][k - 1] + 1} 0\n")
-    if k == 0:
-        for i in range(n):
-            fw(f"-{x[i] + 1} 0\n")
-
-
-def GenMatsuiConstraint(x, u, n, k, l, r, m, fw):
-    if m > 0:
-        if l == 0 and r < n - 1:
-            for i in range(1, r + 1):
-                fw(f"-{x[i] + 1} -{u[i - 1][m - 1] + 1} 0\n")
-        if l > 0 and r == n - 1:
-            for i in range(k - m):
-                fw(f"{u[l - 1][i] + 1} -{u[r - 1][i + m] + 1} 0\n")
-            for i in range(k - m + 1):
-                fw((f"{u[l - 1][i] + 1} -{x[r] + 1} -{u[r - 1][i + m - 1] + 1} 0\n"))
-        if l > 0 and r < n - 1:
-            for i in range(k - m):
-                fw(f"{u[l - 1][i] + 1} -{u[r][i + m] + 1} 0\n")
-    if m == 0:
-        for i in range(l, r + 1):
-            fw(f"-{x[i] + 1} 0\n")
-
-
-def cnfbuilder(round, activeSbox, matsuiRoundIndex, matsuiCount, cnffile):
-    totalSbox = 16 * round
-    count_var = 0
-    xi = [64 * [0] for _ in range(round)]
-    w = [16 * [0] for _ in range(round)]
-    xo = [64 * [0] for _ in range(round)]
-    for i in range(round):
-        for j in range(64):
-            xi[i][j] = count_var
-            count_var += 1
-        for j in range(16):
-            w[i][j] = count_var
-            count_var += 1
-    for i in range(round - 1):
-        for j in range(64):
-            xo[i][j] = xi[i + 1][j]
-    for i in range(64):
-        xo[round - 1][i] = count_var
-        count_var += 1
-    var_u = []
-    for i in range(totalSbox - 1):
-        var_u.append([])
-        for j in range(activeSbox):
-            var_u[i].append(count_var)
-            count_var += 1
-
-    count_clause = CountClausesInRoundFunction(round)
-    mainVarNum = 16 * round
-    cardinalityCons = activeSbox
-    count_clause += CountClausesInSequentialEncoding(mainVarNum, cardinalityCons)
-    for mc in range(matsuiCount):
-        StartingRound = matsuiRoundIndex[mc][0]
-        EndingRound = matsuiRoundIndex[mc][1]
-        leftNode = 16 * StartingRound
-        rightNode = 16 * EndingRound - 1
-        partialCardinalityCons = (
-            activeSbox
-            - DiffActiveSbox[StartingRound]
-            - DiffActiveSbox[round - EndingRound]
-        )
-        count_clause += CountClausesForMatsuiStrategy(
-            mainVarNum,
-            cardinalityCons,
-            leftNode,
-            rightNode,
-            partialCardinalityCons,
-        )
-
-    with open(cnffile, "w") as file:
-        file.write(f"p cnf {count_var} {count_clause}\n")
-        # Add constraints to claim nonzero input difference
-        clauseseq = ""
-        for i in range(64):
-            clauseseq += f"{xi[0][i] + 1} "
-        clauseseq += f"0\n"
-        file.write(clauseseq)
-        # Add constraints for the round function
-        for r in range(round):
-            y = [xo[r][P[i]] for i in range(64)]
-            for i in range(16):
-                for j in range(43):
-                    X = []
-                    for k in range(4):
-                        X.append(xi[r][4 * i + k])
-                    for k in range(4):
-                        X.append(y[4 * i + k])
-                    X.append(w[r][i])
-                    clauseseq = ""
-                    for k in range(9):
-                        if SymbolicCNFConstraintForSbox43[j][k] == 1:
-                            clauseseq += f"-{X[k] + 1} "
-                        if SymbolicCNFConstraintForSbox43[j][k] == 0:
-                            clauseseq += f"{X[k] + 1} "
-                    clauseseq += f"0\n"
-                    file.write(clauseseq)
-        # Add constraints for the original sequential encoding
-        mainVars = []
-        for r in range(round):
-            for i in range(16):
-                mainVars.append(w[round - 1 - r][i])
-        GenSequentialEncoding(mainVars, var_u, mainVarNum, cardinalityCons, file.write)
-        # Add constraints for Matsui's strategy
-        for mc in range(matsuiCount):
-            StartingRound = matsuiRoundIndex[mc][0]
-            EndingRound = matsuiRoundIndex[mc][1]
-            leftNode = 16 * StartingRound
-            rightNode = 16 * EndingRound - 1
-            partialCardinalityCons = (
-                activeSbox
-                - DiffActiveSbox[StartingRound]
-                - DiffActiveSbox[round - EndingRound]
-            )
-            GenMatsuiConstraint(
-                mainVars,
-                var_u,
-                mainVarNum,
-                cardinalityCons,
-                leftNode,
-                rightNode,
-                partialCardinalityCons,
-                file.write,
-            )
-
-
+tf = open(TIME_OUT, "a")
 countSbox = InitialLowerBound
-TotalTimeStart = time.time()
-for totalround in range(SearchRoundStart, SearchRoundEnd):
-    time_start = time.time()
-    matsuiRoundIndex = []
-    matsuiCount = 0
-    # Generate Matsui condition under choice 1
-    if GroupConstraintChoice == 1:
-        for group in range(GroupNumForChoice1):
-            for round in range(1, totalround - group + 1):
-                matsuiRoundIndex.append([group, group + round])
-                matsuiCount += 1
-    # Printing Matsui conditions
-    with open(MATSUICONDITION_PATH, "a") as file:
-        resultseq = f"Round: {totalround}; Partial Constraint Num: {matsuiCount}\n"
-        file.write(resultseq)
-        file.write(f"{matsuiRoundIndex}\n")
-        p(resultseq.strip("\n"))
-        p(f"{matsuiRoundIndex}")
-    flag = False
-    while flag == False:
+tick("total")
+for round in range(SearchRoundStart, SearchRoundEnd):
+    tick("round")
+    matsuiRoundIndex, matsuiCount = matsui(round)
+    while True:
         cnfbuilder(
-            totalround,
+            round,
             countSbox,
             matsuiRoundIndex,
             matsuiCount,
-            f"R{totalround}-A{countSbox}.cnf",
+            SymbolicCNFConstraintForSbox,
+            f"R{round}-A{countSbox}.cnf",
         )
-        ts = time.time()
-        flag = satsolver(
-            f"R{totalround}-A{countSbox}.cnf", f"R{totalround}-A{countSbox}.out"
-        )
-        te = time.time()
-        p(
-            f"\tRound: {totalround}; Active: {countSbox:2}; {'Sat' if flag else 'UnSat':>5}; Cost: {ts - te}"
-        )
+        tick("sat")
+        flag = satsolver(f"R{round}-A{countSbox}.cnf", f"R{round}-A{countSbox}.out")
+        tick("sat")
+        log(f"\tRound: {round}; Active: {countSbox:2}; {flag:1}; Cost: {cost('sat')}")
         countSbox += 1
-    DiffActiveSbox[totalround] = countSbox - 1
-    time_end = time.time()
-    with open(TIME_PATH, "a") as file:
-        file.write(
-            f"Round: {totalround}; Active S-box: {DiffActiveSbox[totalround]}; Runtime: {time_end - time_start}\n"
-        )
-TotalTimeEnd = time.time()
-p(DiffActiveSbox)
-p(f"Total Runtime: {TotalTimeEnd - TotalTimeStart}")
-with open(TIME_PATH, "a") as file:
-    file.write(f"Total Runtime: {TotalTimeEnd - TotalTimeStart}\n\n")
+        if flag:
+            break
+    DiffActiveSbox[round] = countSbox - 1
+    tick("round")
+    tf.write(f"Round: {round}; Active: {countSbox - 1}; Cost: {cost('round')}\n")
+tick("total")
+
+
+tf.write(f"Total Runtime: {cost('total')}\n\n")
+tf.close()
+log(DiffActiveSbox)
+log(f"Total Runtime: {cost('total')}")
